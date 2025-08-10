a65b4df48f730ea332042ecf4149942f53008115
“yingzhang0897” <yinganticpc@gmail.com>
2025-08-10 08:25:50 +0800

    add  cors*

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",     
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 app.use("/api", require("./routes/products"));
 app.use("/api/shipping", require("./routes/shipping"));
